49215deeab65748ce56fea85b96e246da7df071b
thenoen <kordikmarek@gmail.com>
2020-12-20 14:36:39 +0100

    initial commit

---
+++ history.cpp
@@ -0,0 +1,4 @@
+#include "progress.h"
+
+int history_data[120];
+float current_average = 0.0;
---
+++ history.h
@@ -0,0 +1,3 @@
+#include <Arduino.h>
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+
